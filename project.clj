1fbc1473ba7b78d009240f2ca21a762b14a91725
Pavel Tisnovsky <ptisnovs@redhat.com>
2015-06-12 19:04:36 +0200

    Added plugins to the project file

--- project.clj
+++ project.clj
@@ -33,5 +33,8 @@
     :license {:name "Eclipse Public License"
               :url "http://www.eclipse.org/legal/epl-v10.html"}
     :dependencies [[org.clojure/clojure "1.6.0"]]
+    :plugins [[codox "0.8.11"]
+              [test2junit "1.1.0"]
+              [lein-cloverage "1.0.2"]]
     :main cljgfx.core)
 
