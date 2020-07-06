e2815d224091547475adca1e8a7b1795a66211a9
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-07-06 10:05:12 +0200

    Reformatted project.clj by zprint

--- project.clj
+++ project.clj
@@ -28,13 +28,13 @@
 ;;; Project definition file.
 
 (defproject cljgfx "0.1.0-SNAPSHOT"
-    :description "FIXME: write description"
-    :url "http://example.com/FIXME"
-    :license {:name "Eclipse Public License"
-              :url "http://www.eclipse.org/legal/epl-v10.html"}
-    :dependencies [[org.clojure/clojure "1.8.0"]]
-    :plugins [[codox "0.8.11"]
-              [test2junit "1.1.0"]
-              [lein-cloverage "1.0.2"]]
-    :main cljgfx.core)
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License",
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :plugins [[codox "0.8.11"]
+            [test2junit "1.1.0"]
+            [lein-cloverage "1.0.2"]]
+  :main cljgfx.core)
 
