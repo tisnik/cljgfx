3d30b7141256689273ce2fb494af0e52408f90da
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-07-08 08:17:23 +0200

    Reformatted core.clj by zprint

--- src/cljgfx/core.clj
+++ src/cljgfx/core.clj
@@ -32,72 +32,101 @@
 
 (import 'java.awt.Color)
 
-(require '[cljgfx.config :as config])
-(require '[cljgfx.bitmap :as bitmap])
-(require '[cljgfx.calc   :as calc])
-(require '[cljgfx.draw   :as draw])
-(require '[cljgfx.rendering-hints   :as rendering-hints])
+(require '[cljgfx.config          :as config])
+(require '[cljgfx.bitmap          :as bitmap])
+(require '[cljgfx.calc            :as calc])
+(require '[cljgfx.draw            :as draw])
+(require '[cljgfx.rendering-hints :as rendering-hints])
 
 (defn draw-filled-arcs
-    "Draw the first part of pie-chart: filled arcs."
-    [graphics arc-width arc-height results colors start-angles arc-angles]
-    (doseq [i (range (count results))]
-        (draw/fill-arc graphics arc-width arc-height
-            (nth colors i)
-            (nth start-angles i)
-            (nth arc-angles i))))
+  "Draw the first part of pie-chart: filled arcs."
+  [graphics arc-width arc-height results colors start-angles arc-angles]
+  (doseq [i (range (count results))]
+    (draw/fill-arc graphics
+                   arc-width
+                   arc-height
+                   (nth colors i)
+                   (nth start-angles i)
+                   (nth arc-angles i))))
 
 (defn draw-arcs
-    "Draw the second part of pie-chart: hollow arcs."
-    [graphics width height arc-width arc-height results start-angles arc-angles]
-    (.setStroke graphics (new java.awt.BasicStroke 2))
-    (doseq [i (range (count results))]
-        (draw/draw-arc graphics width height arc-width arc-height
-            (nth start-angles i)
-            (nth arc-angles i))))
+  "Draw the second part of pie-chart: hollow arcs."
+  [graphics width height arc-width arc-height results start-angles arc-angles]
+  (.setStroke graphics (new java.awt.BasicStroke 2))
+  (doseq [i (range (count results))]
+    (draw/draw-arc graphics
+                   width
+                   height
+                   arc-width
+                   arc-height
+                   (nth start-angles i)
+                   (nth arc-angles i))))
 
 (defn draw-labels
-    "Draw the third part of pie-chart: labels (texts)."
-    [graphics width height arc-width arc-height results start-angles arc-angles]
-    (doseq [i (range (count results))]
-        (draw/draw-label graphics width height arc-width arc-height
-            (nth start-angles i)
-            (nth arc-angles i) (nth results i))))
+  "Draw the third part of pie-chart: labels (texts)."
+  [graphics width height arc-width arc-height results start-angles arc-angles]
+  (doseq [i (range (count results))]
+    (draw/draw-label graphics
+                     width
+                     height
+                     arc-width
+                     arc-height
+                     (nth start-angles i)
+                     (nth arc-angles i)
+                     (nth results i))))
 
 (defn draw-pie-chart
-    "Draw whole pie-chart onto the selected canvas."
-    [graphics width height results colors]
-    (let [
-          arc-width  (- width  (* config/arc-border 2))
-          arc-height (- height (* config/arc-border 2))
-          total      (reduce + results)
-          arc-angles (map calc/calc-arc-angle results (repeat (count results) total))
-          start-angles (reductions + 0 arc-angles)
-          ]
-          (draw-filled-arcs graphics arc-width arc-height results colors start-angles arc-angles)
-          (draw-arcs graphics width height arc-width arc-height results start-angles arc-angles)
-          (draw-labels graphics width height arc-width arc-height results start-angles arc-angles)
-    ))
+  "Draw whole pie-chart onto the selected canvas."
+  [graphics width height results colors]
+  (let [arc-width  (- width  (* config/arc-border 2))
+        arc-height (- height (* config/arc-border 2))
+        total      (reduce + results)
+        arc-angles (map calc/calc-arc-angle results (repeat (count results) total))
+        start-angles (reductions + 0 arc-angles)]
+    (draw-filled-arcs graphics
+                      arc-width
+                      arc-height
+                      results
+                      colors
+                      start-angles
+                      arc-angles)
+    (draw-arcs graphics
+               width
+               height
+               arc-width
+               arc-height
+               results
+               start-angles
+               arc-angles)
+    (draw-labels graphics
+                 width
+                 height
+                 arc-width
+                 arc-height
+                 results
+                 start-angles
+                 arc-angles)))
 
 (defn generate-pie-chart
-    "Generate new bitmap with pie-chart."
-    [filename results colors]
-    (let [width      config/default-graph-width
-          height     config/default-graph-height
-          bitmap     (bitmap/create-bitmap width height)
-          graphics   (bitmap/get-graphics-2d bitmap)]
-          (draw/clear-canvas graphics width height)
-          (rendering-hints/set-rendering-hints graphics)
-          (draw-pie-chart graphics width height results colors)
-          (bitmap/write-bitmap filename bitmap)
-    ))
+  "Generate new bitmap with pie-chart."
+  [filename results colors]
+  (let [width      config/default-graph-width
+        height     config/default-graph-height
+        bitmap     (bitmap/create-bitmap width height)
+        graphics   (bitmap/get-graphics-2d bitmap)]
+    (draw/clear-canvas graphics width height)
+    (rendering-hints/set-rendering-hints graphics)
+    (draw-pie-chart graphics width height results colors)
+    (bitmap/write-bitmap filename bitmap)))
 
 (defn -main
-    "Entry function to the cljgfx tool."
-    []
-    (println "Generating pie chart")
-    (generate-pie-chart "test1.png" [10 20 30 40] [Color/green Color/yellow Color/red Color/blue])
-    (println "Done"))
+  "Entry function to the cljgfx tool."
+  []
+  (println "Generating pie chart")
+  (generate-pie-chart "test1.png"
+                      [10 20 30 40]
+                      [Color/green Color/yellow Color/red Color/blue])
+  (println "Done"))
 
 ;(-main)
 
